4abadda0debc072ed4f3f1d0b38c423240646e60
BorderKontrol <blade@8chan.co>
2019-01-24 18:29:56 +0100

    enable submit button on select

--- js/client.js
+++ js/client.js
@@ -3,6 +3,8 @@ document.getElementById("authcode").value = window.location.pathname.slice(1);
 var modalimg = document.getElementById("modalimg");
 var modal = document.getElementById("modal");
 var items = document.getElementsByClassName("items");
+var select = document.getElementById("select");
+var submitbtn = document.getElementById("submitbtn");
 
 for(var i = 0; i < items.length; i++){
     var item = items.item(i);
@@ -19,4 +21,8 @@ modal.addEventListener("click", function(event) {
 
 modalimg.addEventListener("load", function(event) {
     modalimg.parentElement.style.display = "block";
+});
+
+select.addEventListener("change"), function(event) {
+    submitbtn.disabled = false;
 });
